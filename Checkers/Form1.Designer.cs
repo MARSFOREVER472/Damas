9d249cb74922a6226c41fca4fe69683c1605c0d2
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-07-12 14:15:48 -0400

    Commit inicial

---
+++ Checkers/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Checkers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 484);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 500);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Juego de Damas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Checkers/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Checkers
+{
+    public partial class Form1 : Form
+    {
+        // Método inicial de la interfaz.
+        public Form1()
+        {
+            InitializeComponent(); // Método de ejecución inicial.
+        }
+
+        // Declaramos variables.
+
+        int n; // Variable numérica para los colores de las casillas.
+        PictureBox[,] P; // Variable para las Casillas del tablero.
+
+        // Se carga la interfaz ejecutable.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            n = 8; // Tamaño máximo del tablero de 8*8 cuadrados diferentes.
+            P = new PictureBox[n, n]; // Se crea el tablero original similar a la de un tablero de ajedrez.
+            int left = 2, top = 2; // 2 casillas desde la izquierda y 2 casillas desde arriba.
+            Color[] colors = new Color[] { Color.White, Color.Black }; // Colores del tablero.
+
+            // EN INSTANTES...
+        }
+    }
+}
